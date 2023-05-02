e7616ce4e5c3501e39ba63ca4fc6cccc945efb73
ManinderManan <manindersmanan@gmail.com>
2023-05-01 22:40:29 -0700

    udpated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ INSERT INTO role (title, salary, department_id)
 VALUE ("Head of Security", 25000.00, 2), ("Scientist", 600000.00, 3), ("Engineer", 600000.00, 4), ("Butler", 200000.00, 1), ("CEO", 800000.00, 5);
 
 INSERT INTO employee (FirstName, LastName, RoleID, ManagerID)
-VALUE ("Happy", "Hogan", 1, 3), ("Anton", "Vanko", 1, 1), ("Tony", "Stark", 3, 2), ("Steve", "Quinn", 5, 2), ("Pepper", "Potts", 5, 2);
+VALUE ("John", "Appleseed", 1, 3), ("Bill", "Gates", 1, 1), ("Steve", "Jobs", 3, 2), ("Jeff", "Bezos", 5, 2), ("Kobe", "Bryant", 5, 2);
